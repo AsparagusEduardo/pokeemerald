c4363cf500b42ba9cabd49b099e5af2202996eec
Eduardo Quezada <eduardo602002@gmail.com>
2025-12-10 16:54:35 -0300

    Intro_WaitForShinyAnimAndHealthbox - Step 9

--- src/battle_controllers.c
+++ src/battle_controllers.c
@@ -3194,23 +3194,18 @@ void BtlController_Intro_WaitForShinyAnimAndHealthbox(u32 battler)
             }
             else
             {
-                if (gBattleSpritesDataPtr->healthBoxesData[battler].finishedShinyMonAnim)
-                {
-                    if (GetBattlerPosition(battler) == B_POSITION_OPPONENT_RIGHT)
-                    {
-                        if (gBattleSpritesDataPtr->healthBoxesData[BATTLE_PARTNER(battler)].triedShinyMonAnim)
-                            return;
-                        if (gBattleSpritesDataPtr->healthBoxesData[BATTLE_PARTNER(battler)].finishedShinyMonAnim)
-                            return;
-                        FreeShinyStars();
-                    }
-                    gBattleSpritesDataPtr->healthBoxesData[battler].triedShinyMonAnim = FALSE;
-                    gBattleSpritesDataPtr->healthBoxesData[battler].finishedShinyMonAnim = FALSE;
-                }
-                else
-                {
+                if (!gBattleSpritesDataPtr->healthBoxesData[battler].finishedShinyMonAnim)
                     return;
+                if (GetBattlerPosition(battler) == B_POSITION_OPPONENT_RIGHT)
+                {
+                    if (gBattleSpritesDataPtr->healthBoxesData[BATTLE_PARTNER(battler)].triedShinyMonAnim)
+                        return;
+                    if (gBattleSpritesDataPtr->healthBoxesData[BATTLE_PARTNER(battler)].finishedShinyMonAnim)
+                        return;
+                    FreeShinyStars();
                 }
+                gBattleSpritesDataPtr->healthBoxesData[battler].triedShinyMonAnim = FALSE;
+                gBattleSpritesDataPtr->healthBoxesData[battler].finishedShinyMonAnim = FALSE;
             }
 
             gBattleSpritesDataPtr->healthBoxesData[battler].introEndDelay = 3;
