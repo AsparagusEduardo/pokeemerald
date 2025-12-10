96f1bb95e26eb1b15ba0681a64acb2a7d3856eda
Eduardo Quezada <eduardo602002@gmail.com>
2025-12-10 20:54:16 -0300

    fix wally

--- src/battle_controllers.c
+++ src/battle_controllers.c
@@ -96,6 +96,11 @@ bool32 BattlerIsLink(u32 battlerId)
          || gBattlerBattleController[battlerId] == BATTLE_CONTROLLER_LINK_OPPONENT);
 }
 
+bool32 BattlerIsWally(u32 battlerId)
+{
+    return (gBattlerBattleController[battlerId] == BATTLE_CONTROLLER_WALLY);
+}
+
 bool32 BattlerHasAi(u32 battlerId)
 {
     switch (gBattlerBattleController[battlerId])
