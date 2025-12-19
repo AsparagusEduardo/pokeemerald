rh-hideout/pokeemerald-expansion

Handled every possible assume for move info

test/battle/move_effect/speed_down.c
@@ -10,7 +10,8 @@ DOUBLE_BATTLE_TEST("Speed Down: Cotton Spore does not fail if it is blocked by o
 
     GIVEN {
         ASSUME(GetMoveEffect(MOVE_COTTON_SPORE) == EFFECT_SPEED_DOWN_2);
-        ASSUME(GetMoveTarget(MOVE_COTTON_SPORE) == MOVE_TARGET_BOTH);
+        WITH_MOVE_DATA(MOVE_COTTON_SPORE, MOVE_DATA_TARGET, MOVE_TARGET_BOTH);
+        WITH_CONFIG(GEN_CONFIG_POWDER_OVERCOAT, GEN_6);
         PLAYER(SPECIES_WOBBUFFET);
         PLAYER(SPECIES_WOBBUFFET);
         OPPONENT(SPECIES_SHELLDER) { Ability(abilityOne); }

AsparagusEduardo/pokeemerald

test/battle/move_effect/speed_down.c
@@ -10,7 +10,8 @@ DOUBLE_BATTLE_TEST("Speed Down: Cotton Spore does not fail if it is blocked by o
 
     GIVEN {
         ASSUME(GetMoveEffect(MOVE_COTTON_SPORE) == EFFECT_SPEED_DOWN_2);
-        ASSUME(GetMoveTarget(MOVE_COTTON_SPORE) == MOVE_TARGET_BOTH);
+        WITH_MOVE_DATA(MOVE_COTTON_SPORE, MOVE_DATA_TARGET, MOVE_TARGET_BOTH);
+        WITH_CONFIG(CONFIG_POWDER_OVERCOAT, GEN_6);
         PLAYER(SPECIES_WOBBUFFET);
         PLAYER(SPECIES_WOBBUFFET);
         OPPONENT(SPECIES_SHELLDER) { Ability(abilityOne); }